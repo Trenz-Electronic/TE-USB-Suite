b1b737f497e649e3ea93c30a8bf9d8709025b272
Fabio  De Riccardis <Fabio.DeRiccardis@mirifica.it>
2012-04-21 16:30:54 +0200

    renamed TE-USB-FX2 to TE_USB_FX2.firmware

--- TE-FX2-FW/prepare.bat
+++
@@ -1,9 +0,0 @@
-@mkdir Cypress
-@cd Cypress
-@install\wget.exe -O cy3684_ez_usb_fx2lp_development_kit_15.exe http://www.cypress.com/?docID=6018
-@echo "Press any key to start EZ-USB Development Kit installation"
-@echo "Install to default (C:\Cypress\USB) destination folder"
-@echo "Select Typical instalation"
-@pause
-@start cy3684_ez_usb_fx2lp_development_kit_15.exe
-@cd ..
